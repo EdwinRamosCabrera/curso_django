781a76abb9128417e89f2357e4a1aa6b13a250dd
Edwin Ramos <edwin_ramos2@usmp.pe>
2025-10-09 23:23:21 -0500

    Add orders

--- coffee_shop_platzi/coffee_shop_platzi/settings.py
+++ coffee_shop_platzi/coffee_shop_platzi/settings.py
@@ -42,6 +42,7 @@ INSTALLED_APPS = [
     'crispy_tailwind',
     'products',
     'users',
+    'orders',
 ]
 
 MIDDLEWARE = [
@@ -132,3 +133,4 @@ CRISPY_TEMPLATE_PACK = "tailwind"
 
 LOGIN_REDIRECT_URL = 'list_product'
 LOGOUT_REDIRECT_URL = 'list_product'
+LOGIN_URL = 'login'
