958d3c036de6c6d9018dd1ec7693eb804fe9b13c
Edwin Ramos <edwin_ramos2@usmp.pe>
2025-10-12 11:16:11 -0500

    Add RestFramework

--- coffee_shop_platzi/products/urls.py
+++ coffee_shop_platzi/products/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from .views import ProductFormView, ProductListView
+from .views import ProductFormView, ProductListAPI, ProductListView
 
 urlpatterns = [
     path('', ProductListView.as_view(), name='list_product'),
     path('agregar/', ProductFormView.as_view(), name='add_product'),
+    path('api/products/', ProductListAPI.as_view(), name='api_product_list'),
 ] 
--- coffee_shop_platzi/products/views.py
+++ coffee_shop_platzi/products/views.py
@@ -1,8 +1,11 @@
 from django.shortcuts import render
 from django.views import generic
 from django.urls import reverse_lazy
+from rest_framework.views import APIView
+from rest_framework.response import Response
 from .forms import ProductForm
 from .models import Product
+from .serializers import ProductSerializer
 
 
 class ProductFormView(generic.FormView):
@@ -19,4 +22,15 @@ class ProductListView(generic.ListView):
     model = Product
     template_name = "products/list_product.html"
     context_object_name = "products"
+
+class ProductListAPI(APIView):
+    authentication_classes = [] # Deshabilitar la autenticación para esta vista
+    permission_classes = [] # Deshabilitar los permisos para esta vista
+    
+    def get(self, request):
+        products = Product.objects.all()
+        serializer = ProductSerializer(products, many=True)
+        return Response(serializer.data)
+
+   
     
